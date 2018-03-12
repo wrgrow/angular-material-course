63401b30057e8fb2010710d8bdbdc0eb7c9ed569
bill grow <billgrowjr@hotmail.com>
2018-03-12 14:19:57 -0400

    coursecardlist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,17 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        AboutComponent
+        AboutComponent,
+        TopMenuComponent,
+        CoursesCardListComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { Course } from '../model/course';
+@Component({
+  selector: 'courses-card-list',
+  templateUrl: './courses-card-list.component.html',
+  styleUrls: ['./courses-card-list.component.css']
+})
+export class CoursesCardListComponent implements OnInit {
+  @Input()
+  courseList: Course[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
